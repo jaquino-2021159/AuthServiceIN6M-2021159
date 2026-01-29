58aa1e1ea3f6c5f83e3ea939a8b2ee5304e5dd67
Informatica <informatica@alumno.local>
2026-01-29 11:51:04 -0600

    Se agrega continuacion de Services

--- AuthServiceIN6BM/src/AuthServiceIN6BM.Persistence/obj/Debug/net8.0/AuthServiceIN6BM.Persistence.AssemblyInfo.cs
+++ AuthServiceIN6BM/src/AuthServiceIN6BM.Persistence/obj/Debug/net8.0/AuthServiceIN6BM.Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthServiceIN6BM.Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e9c94c36fd7f60e7a8e87df288e0565c4400c86")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8e00a3c058cce5dfe8e273a6394f60ce8ce57c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthServiceIN6BM.Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthServiceIN6BM.Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
